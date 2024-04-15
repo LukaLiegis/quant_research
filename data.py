c178ab0fac5ee1a507379885d3e19f2b6f130686
LukaLiegis <luka.liegis@gmail.com>
2024-04-15 17:24:26 +0300

    add day of month column

--- data.py
+++ data.py
@@ -2,10 +2,15 @@ import yfinance as yf
 from numba import jit
 import pytz
 
-@jit(nopython=True)
+
 def get_data(ticker):
+    """
+    Get data from yahoo finance
+    :param ticker:
+    :return: dataframe
+    """
     df = yf.Ticker(ticker).history(period="5y", auto_adjust=True).reset_index()
-    df = df.rename(columns = {
+    df = df.rename(columns={
         "Date": "datetime",
         "Open": "open",
         "High": "high",
@@ -14,10 +19,11 @@ def get_data(ticker):
         "Volume": "volume"
     })
     df["datetime"] = df["datetime"].dt.tz_convert(pytz.utc)
+    df["dom"] = df["datetime"].dt.day
     # Calculate the percent change os the close data
     df["returns"] = df["close"].pct_change()
-    df = df.drop(columns=["Dividends", "Stock Splits"])
-    df = df.set_index("datetime", drop = True)
-    df = df.dropna() 
+    df = df.drop(columns=["Dividends", "Stock Splits", "Capital Gains"])
+    df = df.set_index("datetime", drop=True)
+    df = df.dropna()
     print("The size of ", ticker, "is ", df.shape)
     return df
